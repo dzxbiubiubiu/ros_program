65db502d9abb80f8bd77bb577ceff5dd1d0ab3fa
dzxbiubiubiu <406953326@qq.com>
2020-04-06 20:15:38 +0800

    ros topic program

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/dzx/omtb_map_extension_plugin/devel/lib:/home/dzx/catkin_ws_omtb/devel/lib:/home/dzx/carto_turtlebot/devel/lib:/home/dzx/carto_ws/install_isolated/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/home/dzx/omtb_map_extension_plugin/devel/lib/pkgconfig:/home/dzx/catkin_ws_omtb/devel/lib/pkgconfig:/home/dzx/carto_turtlebot/devel/lib/pkgconfig:/home/dzx/carto_ws/install_isolated/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/dzx/workspace/ros_program/catkin_ws/build"
-export ROS_PACKAGE_PATH="/home/dzx/workspace/ros_program/catkin_ws/src:/home/dzx/omtb_map_extension_plugin/src:/home/dzx/catkin_ws_omtb/src:/home/dzx/carto_turtlebot/src:/home/dzx/carto_ws/install_isolated/share:/opt/ros/kinetic/share"
