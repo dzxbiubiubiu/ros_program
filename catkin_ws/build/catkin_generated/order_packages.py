184ee9fca2bbe94e79a11367e62fe22d47a3ef4f
dzxbiubiubiu <406953326@qq.com>
2020-04-06 17:47:06 +0800

    first ros commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dzx/workspace/ros_program/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/dzx/workspace/ros_program/catkin_ws/devel;/home/dzx/omtb_map_extension_plugin/devel;/home/dzx/catkin_ws_omtb/devel;/home/dzx/carto_turtlebot/devel;/home/dzx/carto_ws/install_isolated;/opt/ros/kinetic".split(';') if "/home/dzx/workspace/ros_program/catkin_ws/devel;/home/dzx/omtb_map_extension_plugin/devel;/home/dzx/catkin_ws_omtb/devel;/home/dzx/carto_turtlebot/devel;/home/dzx/carto_ws/install_isolated;/opt/ros/kinetic" != "" else []
